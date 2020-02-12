19af374ebd3f985d14d04b0382fbd0485811e93f
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-02-11 23:34:26 -0600

    react app up and running

---
+++ client/src/components/Users.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+function Users(props) {
+    return (
+        <div>
+            {props.users.map(user => {
+            console.log(user)
+          return <Link to={`/users/${user.id}`}>
+            <div>Username: {user.name}</div>
+          </Link>
+        })}
+        </div>
+    );
+}
+
+export default Users;
